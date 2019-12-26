d1f8d10d2cb2d2baa2d5a9a4d3b93da289247962
Shenzhen YAO <junk.them@hotmail.com>
2019-12-25 20:14:04 -0600

    153f

--- js/components.js
+++ js/components.js
@@ -5,7 +5,8 @@ function addtitledesc (titletext, gitcommitver, githuburl){
 	titlediv
 	.append('p')
     .attr('class', 'pagehead')
-    .styles({'font-size': '60px', 'font-weight': '200', 'font-family': "'Source Sans Pro'"})
+    // .styles({'font-size': '60px', 'font-weight': '200', 'font-family': "'Source Sans Pro'"})
+    .styles({'font-size': '48px', 'font-weight': 'bold', 'font-family': "Lora"})
     .text(titletext )
     .append('span')
     .styles({'font-size': '12px', 'font-weight': 'normal'})
