13d21b6b7f90c1989d6fa96a7bc5a3fc1030a8a2
Shenzhen YAO <junk.them@hotmail.com>
2020-03-29 10:47:00 -0600

    160a_2020-03-29

--- gitcommit.cmd
+++ gitcommit.cmd
@@ -1,5 +1,5 @@
 f:
 cd F:\Personal\Virtual_Server\PHPWeb\d3egp2019\160a
 git add .
-git commit -m "160a"
+git commit -m "160a_%date%"
 git push origin master
---
+++ test.cmd
@@ -0,0 +1,10 @@
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%month%_%day%_%year%
+echo datestr is %datestr%
+echo "160a_%date%"
+pause
