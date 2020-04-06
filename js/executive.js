99603402f08a668764783d054b7131c549349154
Shenzhen YAO <junk.them@hotmail.com>
2020-04-05 18:09:01 -0600

    1a

--- js/executive.js
+++ js/executive.js
@@ -346,19 +346,19 @@ function makeSvgRectGTree(){
     // treeData = getJsonFromsessionStorage;
     // NewTree (treeData)
     
-
+// change!!!
     //when running on local host:
-    if (currentURL.startsWith("http://localhost:")){
+    // if (currentURL.startsWith("http://localhost:")){
 
         // load the default tree data stored in the data folder (which is set in init.js)
         newTreebyJsonfromURL(treejsonURL)
 
-    } else {
-        //when running online, load the default json either from workaround (on netlify) or from a json str hidden in PHP
-        var loadedjsonstr = sessionStorage.getItem("thejsonstr");
-        treeData = JSON.parse(loadedjsonstr)
-        NewTree (treeData)
-    }
+    // } else {
+    //     //when running online, load the default json either from workaround (on netlify) or from a json str hidden in PHP
+    //     var loadedjsonstr = sessionStorage.getItem("thejsonstr");
+    //     treeData = JSON.parse(loadedjsonstr)
+    //     NewTree (treeData)
+    // }
 
     // // the following illustrates the timing of loading json, making tree, and the value of treeData, and root data point 
     // // initially it cannot be seen because of the asynchoronous settings.
